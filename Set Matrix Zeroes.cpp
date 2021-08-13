3e02184a770f522974987b2c5ae16e9ead249ef2
Tejas Deore <tejasdeore80@gmail.com>
2021-08-13 18:28:01 +0530

    Create Set Matrix Zeroes.cpp

---
+++ Set Matrix Zeroes.cpp
@@ -0,0 +1,83 @@
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+//#define ordered_set tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define ar array
+#define ld long double
+
+// A hash function used to hash a pair of any kind
+struct hash_pair {
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2>& p) const
+    {
+        auto hash1 = hash<T1>{}(p.first);
+        auto hash2 = hash<T2>{}(p.second);
+        return hash1 ^ hash2;
+    }
+};
+
+// Comparator function by key for MAP
+struct cmpmap {
+    bool operator()(const string& a, const string& b) const {
+        return a.length() < b.length();
+    }
+};
+
+// Comparator function for PRIORITY QUEUE
+struct cmppq {
+    bool operator()(const string& p1, const string& p2){
+        return p1.length() < p2.length();
+    }
+};
+
+const int mod= (int)1e9 + 7;
+const int mxN = 200005;
+const int INF = INT_MAX;
+#define pb push_back
+#define forn(i,j,n) for(int (i)=(j);(i) >= (n); (i)--)
+#define forr(i,j,n) for(int (i)=(j);(i) < (n); (i)++)
+#define sz(x) ((int)(x).size())
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef map<int, int> mp;
+typedef unordered_map<int,int> ump;
+#define all(v) v.begin(), v.end() 
+#define alla(arr, sz) arr, arr + sz 
+#define sort(v) sort(all(v))
+#define sorta(arr, sz) sort(alla(arr, sz))  
+
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int r = sz(matrix),c = sz(matrix[0]);
+        
+        bool fr=0,fc=0;
+        forr(i,0,r)if(matrix[i][0]==0)fc=1;
+        forr(j,0,c)if(matrix[0][j]==0)fr=1;
+        forr(i,1,r){
+            forr(j,1,c){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+            
+        forr(i,1,r){
+            forr(j,1,c){
+                if(matrix[i][0]==0 || matrix[0][j]==0)matrix[i][j]=0;
+            }
+        }
+        
+        if(fr==1){
+            forr(j,0,c)matrix[0][j]=0;
+        }
+        
+        if(fc==1){
+            forr(i,0,r)matrix[i][0]=0;
+        }
+        
+    }
+};
