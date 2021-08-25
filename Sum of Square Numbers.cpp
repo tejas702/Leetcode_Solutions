62f5fb407f6f4d0fbd22587bfda8931cc052f475
Tejas Deore <tejasdeore80@gmail.com>
2021-08-25 15:18:12 +0530

    Create Sum of Square Numbers.cpp

---
+++ Sum of Square Numbers.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        unordered_map<int,int> m;
+        for(int i = 0;i <= 46340; i++){
+            m[(i*i)]++;
+            if(m.find(c - (i*i))!=m.end())return true;
+        }
+        return false;        
+    }
+};
